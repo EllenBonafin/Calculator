c27a19d6a9d71ad9461218572cdc4dbc914b8829
Ellen <81322049+EllenBonafin@users.noreply.github.com>
2022-09-15 17:33:14 -0300

    Ifunction

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,28 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Calculator.css'
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
 
 export default function Calculator() {
-    return(
-      <div>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>  
-          <button>,</button>
-          <button>AC</button>
-          <button>+/-</button>
-          <button>%</button>
-          <button>/</button>
-          <button>X</button>
-          <button>-</button>
-          <button>+</button>
-          <button>=</button>
-      </div>
-    )
+  const [num,setNum] = useState(10);
+
+  function inputNum(valor){
+    console.log(valor);
+    //setNum(valor);
+  }
+  return (
+    <div>
+      <Box m={5} />
+      <Container maxWidth="xs">
+        <div className="wrapper">
+          <Box m={12}/>
+          <h1 className="Result">{num}</h1>
+          <button className="ligth-grey">AC</button>
+          <button className="ligth-grey">+/-</button>
+          <button className="ligth-grey">%</button>
+          <button className="Org">/</button>
+
+          <button className="Dark-grey" onClick={inputNum}>7</button>
+          <button className="Dark-grey">8</button>
+          <button className="Dark-grey">9</button>
+          <button className="Org">X</button>
+
+          <button className="Dark-grey">4</button>
+          <button className="Dark-grey">5</button>
+          <button className="Dark-grey">6</button>
+          <button className="Org">-</button>
+
+          <button className="Dark-grey">1</button>
+          <button className="Dark-grey">2</button>
+          <button className="Dark-grey">3</button>
+          <button className="Org">+</button>
+
+          <button className="Dark-grey">0</button>
+          <button className="Dark-grey">,</button>
+          <button className="Dark-grey" style={{ visibility: 'hidden' }}>
+            ,
+          </button>
+          <button className="Org">=</button>
+        </div>
+      </Container>
+    </div>
+  )
 }
