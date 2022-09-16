a7e9a4020f2e051143d28890d0bef51288be3129
Ellen <81322049+EllenBonafin@users.noreply.github.com>
2022-09-16 16:41:39 -0300

    Onclick

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -4,15 +4,16 @@ import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 
 export default function Calculator() {
-  const [num,setNum] = useState(10);
+  const [num,setNum] = useState(5);
 
-  function inputNum(valor){
-    console.log(valor);
+  function inputNum(e){
     //setNum(valor);
+    var input= e.target.value
+    setNum(input);
   }
   return (
     <div>
-      <Box m={5} />
+      <Box m={5}/>
       <Container maxWidth="xs">
         <div className="wrapper">
           <Box m={12}/>
@@ -22,22 +23,22 @@ export default function Calculator() {
           <button className="ligth-grey">%</button>
           <button className="Org">/</button>
 
-          <button className="Dark-grey" onClick={inputNum}>7</button>
-          <button className="Dark-grey">8</button>
-          <button className="Dark-grey">9</button>
+          <button className="Dark-grey" onClick={inputNum}value={7}>7</button>
+          <button className="Dark-grey"onClick={inputNum}value={8}>8</button>
+          <button className="Dark-grey"onClick={inputNum}value={9}>9</button>
           <button className="Org">X</button>
 
-          <button className="Dark-grey">4</button>
-          <button className="Dark-grey">5</button>
-          <button className="Dark-grey">6</button>
+          <button className="Dark-grey"onClick={inputNum}value={4}>4</button>
+          <button className="Dark-grey" onClick={inputNum}value={5}>5</button>
+          <button className="Dark-grey"onClick={inputNum}value={6}>6</button>
           <button className="Org">-</button>
 
-          <button className="Dark-grey">1</button>
-          <button className="Dark-grey">2</button>
-          <button className="Dark-grey">3</button>
+          <button className="Dark-grey"onClick={inputNum}value={1}>1</button>
+          <button className="Dark-grey"onClick={inputNum}value={2}>2</button>
+          <button className="Dark-grey"onClick={inputNum}value={3}>3</button>
           <button className="Org">+</button>
 
-          <button className="Dark-grey">0</button>
+          <button className="Dark-grey"onClick={inputNum}value={0}>0</button>
           <button className="Dark-grey">,</button>
           <button className="Dark-grey" style={{ visibility: 'hidden' }}>
             ,
